fc5a3865621b49bb219be69d0bb9a06356351f0c
Julia Romanowska <Julia.Romanowska@uib.no>
2020-09-25 10:01:57 +0200

    first drake plan!

--- exercises/ex1.R
+++ exercises/ex1.R
@@ -11,43 +11,50 @@ library(dplyr)
 library(purrr)
 
 # Read Traffic Volume dataset ---------------------------------------------
-data <- read.csv('data/Metro_Interstate_Traffic_Volume.csv')
-
-data %>%
-  modify_if(is_character, as.factor) %>%
-  summary
-
+input_file <- 'data/Metro_Interstate_Traffic_Volume.csv'
+read_d <- function(input_file){
+  data <- read.csv(input_file)
+  return(data)
+}
 
 # Preprocess data ---------------------------------------------------------
-data <- data %>%
-  mutate(date = as.Date(date_time),
-         hour = lubridate::hour(date_time))
-
-data %>%
-  filter(holiday != 'None')
-
-data <- data %>%
-  group_by(date) %>%
-  mutate(day_type = case_when(any(holiday != 'None') ~ 'Holiday',
-                              lubridate::wday(date, week_start = 1) >= 6 ~ 'Weekend',
-                              T  ~ 'Weekday')) %>%
-  ungroup()
-
-data <- data %>%
-  select(day_type, temp, hour, traffic_volume)
-
-data$day_type <- as.factor(data$day_type)
-
-summary(data)
-
-data <- data %>% filter(temp > 0)
-
+preproc_d <- function(d){
+  data <- d %>%
+    mutate(date = as.Date(date_time),
+           hour = lubridate::hour(date_time))
+  
+  # data %>%
+    # filter(holiday != 'None')
+  
+  data <- data %>%
+    group_by(date) %>%
+    mutate(day_type = case_when(any(holiday != 'None') ~ 'Holiday',
+                                lubridate::wday(date, week_start = 1) >= 6 ~ 'Weekend',
+                                T  ~ 'Weekday')) %>%
+    ungroup()
+  
+  data <- data %>%
+    select(day_type, temp, hour, traffic_volume)
+  
+  data$day_type <- as.factor(data$day_type)
+  
+  # summary(data)
+  
+  data <- data %>% filter(temp > 0)
+  return(data)
+}
 # Fit model ---------------------------------------------------------------
-library(mgcv)
+fit_and_test <- function(data, gam_k){
+  library(mgcv)
+  
+  model <- gam(traffic_volume ~ s(hour, k = gam_k) + day_type + temp, data = data)
+  
+  # Predict test set --------------------------------------------------------
+  test_predictions <- data %>%
+    mutate(prediction = predict(model, .))
+  return(list(model = model, test = test_predictions))
+}
 
-model <- gam(traffic_volume ~ s(hour, k = -1) + day_type + temp, data = data)
-
-# Predict test set --------------------------------------------------------
-test_predictions <- data %>%
-  mutate(prediction = predict(model, .))
-
+data <- read_d(input_file)
+data_preproc <- preproc_d(data)
+out <- fit_and_test(data_preproc, -1)
--- exercises/ex2.R
+++ exercises/ex2.R
@@ -1,3 +1,71 @@
 # EXERCISE 2
 #
 # TASK: Transform results of Exercise 1 into drake_plan() and execute the plan.
+
+library(drake)
+
+library(dplyr)
+library(purrr)
+library(mgcv)
+
+# Functions ---------------------------------------------------------------
+
+preprocessData <- function(data) {
+  data %>%
+    filter(temp > 0) %>%
+    mutate(date = as.Date(date_time),
+           hour = lubridate::hour(date_time)) %>%
+    group_by(date) %>%
+    mutate(day_type = case_when(any(holiday != 'None') ~ 'Holiday',
+                                lubridate::wday(date, week_start = 1) >= 6 ~ 'Weekend',
+                                T  ~ 'Weekday')) %>%
+    ungroup() %>%
+    mutate(day_type = as.factor(day_type)) %>%
+    select(day_type, temp, hour, traffic_volume)
+}
+
+fitModel <- function(data, gam_k) {
+  gam(traffic_volume ~ s(hour, k = gam_k) + day_type + temp, data = data)
+}
+
+predictNewData <- function(model, newdata) {
+  newdata %>%
+    mutate(prediction = predict(model, .))
+}
+
+
+# Analysis ----------------------------------------------------------------
+data_in <- readr::read_csv('data/Metro_Interstate_Traffic_Volume.csv')
+my_plan <- drake_plan(
+  # Data
+  data = preprocessData(data_in),
+  
+  # Model
+  model = fitModel(data, gam_k = -1),
+  
+  #Performance
+  test_predictions = predictNewData(model, data)
+)
+
+make(my_plan)
+readd(test_predictions)
+
+clean()
+make(my_plan)
+
+vis_drake_graph(my_plan)
+clean(model)
+vis_drake_graph(my_plan)
+
+make(my_plan)
+vis_drake_graph(my_plan)
+
+fitModel <- function(data, gam_k) {
+  gam(traffic_volume ~ s(hour, k = gam_k) + day_type, data = data)
+}
+vis_drake_graph(my_plan)
+outdated(my_plan)
+make(my_plan)
+vis_drake_graph(my_plan)
+
+# nothing happens if I just edit the file, I need to read it in after editing
